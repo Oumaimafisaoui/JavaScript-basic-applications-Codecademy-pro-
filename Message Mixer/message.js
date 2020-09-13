3c3c5d7c31f9025dfc579d1257dbf008fcb1d2c3
GUESS 2B <48260689+Oumaimafisaoui@users.noreply.github.com>
2020-09-13 13:15:52 +0100

    Create message.js

---
+++ Message Mixer/message.js
@@ -0,0 +1,14 @@
+import {countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin} from './messageMixer';
+
+function displayMessage() {
+console.log(countCharacter("What is the color of the sky?", "t"));
+console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+console.log(reverseWord("What is the color of the sky?"));
+console.log(reverseAllWords("What is the color of the sky?"));
+console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+console.log(encode("What is the color of the sky?")); 
+console.log(palindrome("What is the color of the sky?"));
+console.log(pigLatin("What is the color of the sky?", "h"));
+}
+
+displayMessage();
